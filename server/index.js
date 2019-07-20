0d41343db9385847855b57b67b88f6da8ac99398
jjklee <jjlee720@gmail.com>
2019-07-19 17:08:07 -0700

    [prog] pipes get request for component-jj

--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const parser = require('body-parser');
 const path = require('path');
-var cors = require('cors')
+// const cors = require('cors')
+const request = require('request');
 const app = express();
 const port = 3000;
-
 app.use(parser.json());
-app.use(cors())
+// app.use(cors())
 
 parser.urlencoded({ extended: true });
 
 app.use(express.static(path.join(__dirname, '../client')));
+
+app.get('/api/product/:id', (req, res) => {
+  const id = req.params.id
+  const newurl = `http://localhost:3003/api/product/${id}`;
+  request(newurl).pipe(res);
+});
 
 app.listen(port, () => console.log('Listening on port ', port));
